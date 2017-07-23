7b7067f8db9628b7cf298c2d74bc1c8fcfb8e215
magl <823358533@qq.com>
2017-07-23 18:28:19 +0800

    App Pro

---
+++ Yixin_Taoche_App/src/Yixin/Taoche/Util/Constant.java
@@ -0,0 +1,8 @@
+package Yixin.Taoche.Util;
+
+public class Constant {
+	// 定义用户名常量
+	public static final String UserName = "15911051111";
+	// 定义密码常量
+	public static final String Password = "111";
+}
