e26946cb8a2e40226afdabd358a2f4f9aab9ba5a
maguanglu <823358533@qq.com>
2017-07-24 21:14:46 +0800

    判断应用是否是第一次安装

--- Yixin_Taoche_App/src/Yixin/Taoche/Modules/App_BaseCase.java
+++ Yixin_Taoche_App/src/Yixin/Taoche/Modules/App_BaseCase.java
@@ -2,15 +2,18 @@ package Yixin.Taoche.Modules;
 
 import java.io.File;
 import java.net.URL;
+import java.util.NoSuchElementException;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Reporter;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import Yixin.Taoche.Util.App_common;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.remote.MobileCapabilityType;
@@ -20,7 +23,7 @@ public class App_BaseCase {
 
 	@BeforeMethod(alwaysRun = true)
 	public void beforeMethod() throws Exception {
-		File app = new File("E:\\Work\\Yixin\\package\\taoche.apk");
+		File app = new File("APK/taoche.apk");
 		// File app = new File("APK/taoche.apk");
 		// 设置自动化相关参数
 		DesiredCapabilities capabilities = new DesiredCapabilities();
@@ -44,7 +47,7 @@ public class App_BaseCase {
 		// 支持中文输入
 		capabilities.setCapability("unicodeKeyboard", "True");
 		// 重置输入法为系统默认
-		//capabilities.setCapability("restKeyboard", "True");
+		// capabilities.setCapability("restKeyboard", "True");
 		// 安装时不对apk进行重签名，设置很有必要，否则有的apk在重签名后无法正常使用
 		// capabilities.setCapability("noSign", "True");
 		driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"),
@@ -53,35 +56,21 @@ public class App_BaseCase {
 		System.out.println("启动APP");
 		// driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
 		Thread.sleep(5000);
+
+		// 通过定位首页轮播图元素，判断是否为第一次安装
 		/*
-		 * WebDriverWait wait = new WebDriverWait(driver, 20);
-		 * wait.until(ExpectedConditions
-		 * .presenceOfElementLocated(By.name("登录")));
+		 * WebElement IndexPage = driver .findElement(By .xpath(
+		 * "//android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v4.view.ViewPager/android.widget.ImageView"
+		 * ));
 		 */
-		// 获取应用占屏幕大小
-		/*int width = driver.manage().window().getSize().getWidth();
-		int height = driver.manage().window().getSize().getHeight();
-
-		int startx = width * 7 / 8;
-		int endx = width * 1 / 8;
-		int y = height * 1 / 2;
-
-		// 每次启动时，有启动应用的闪屏页，需向左滑动3次
-		try {
-			for (int i = 0; i < 3; i++) {
-				driver.swipe(startx, y, endx, y, 2000);
-				Thread.sleep(500);
-			}
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
+		if (driver.getPageSource().contains("com.taoche.yixin.app:id/vp_guide")) {
+			// 第一次安装需要执行滑动页面操作
+			App_common app_common = new App_common();
+			app_common.Splash_screen(driver);
 		}
-
-		// 最后一个闪屏页找不到可定位元素，所以找到image的classname执行点击操作进入APP首页
-		driver.findElement(By.className("android.widget.ImageView")).click();*/
-
-		// 进入首页后等待界面元素加载完成，再执行其他操作  
+		// 进入首页后等待界面元素加载完成，再执行其他操作
 		// 定位下部导航栏的"二手车"按钮
+
 		WebElement tv_old_car = driver.findElement(By
 				.id("com.taoche.yixin.app:id/tv_old_car"));
 		WebDriverWait wait = new WebDriverWait(driver, 10);
